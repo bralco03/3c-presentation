13a3760861cd3ceefd9519dfe45b9c7962a33bf6
bralco03 <bralco03@hotmail.com>
2025-11-14 18:57:20 -0800

    chore: first commit including revealjs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('camsoft-presentation');
+export class App implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    let deck = new Reveal({
+      plugins: [Markdown],
+    });
+    deck.initialize({transition: 'slide'}).then(() => {
+      console.log('HELLO?')
+    });
+  }
 }
