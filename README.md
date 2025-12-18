96df205edab39614d9bab1ccd00cb8b71fbd8341
bralco03 <bralco03@hotmail.com>
2025-12-18 00:41:31 -0700

    feat: github pages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CamsoftPresentation
+# 3carq-presentation
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.1.5.
 
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, camsoft-presentation');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 3carq-presentation');
   });
 });
